7c8dfeb3b4f2df4c91c5d70ecb1e8a041bdedd38
sudeepsubedi99 <50973728+sudeepsubedi99@users.noreply.github.com>
2021-05-18 23:57:22 +0545

    breeze install

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Thikxa</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div>
+        <div class="text-center sm:text-left bg-gray-800"> 
+            <h1 class="bg-green-300">hi</h1>
+        </div>
+    </div>
+</body>
+</html>
